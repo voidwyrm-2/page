5e297ff8170ecf4464031166b8b34b3e4a04edc1
Radioactivity <85001747+voidwyrm-2@users.noreply.github.com>
2025-09-04 00:04:54 -0400

    Updated spacing in libraries/libstrings

--- src/libraries/libstrings.nim
+++ src/libraries/libstrings.nim
@@ -14,6 +14,7 @@ template addV(name: string, item: NpsValue) =
 template addF(name: string, args: openArray[NpsType], s, r, body: untyped) =
   addF(lib, name, args, s, r, body)
 
+
 addF("chars", @[tString], s, _):
   let str = String(s.pop()).value()
 
